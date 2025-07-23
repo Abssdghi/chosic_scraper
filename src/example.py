9eacd5b3b2ee159ea1b7918cd1d064de1519da2b
abssdghi <abbas.sadeghi1384@gmail.com>
2025-07-23 19:28:22 +0330

    fix example.py bug

--- src/example.py
+++ src/example.py
@@ -7,7 +7,7 @@ id = song['id']
 
 print(f"\nSongs similar to {name} by {artist}:\n")
 
-tracks = get_recommendations(id=id, limit=10)['data']['tracks']
+tracks = get_similar_songs(id=id, limit=10)['data']['tracks']
     
 for i in tracks:
     print(i['artists'][0]['name'] + " - " + i['name'])
